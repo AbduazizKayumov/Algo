fa94094b6eab9494065df0950c71571cb9a6d17e
Abduaziz Kayumov <kayumovabduaziz@gmail.com>
2020-01-30 10:42:05 +0900

    cpp hello

---
+++ python/codeforces/code81111.py
@@ -0,0 +1,18 @@
+t = int(input())
+digits = [
+    [6, 9],
+    [7, 8],
+    [3, 7],
+    [6, 6],
+    [5, 5],
+    [4, 4],
+    [5, 3],
+    [5, 2],
+    [2, 1],
+    [6, 0]
+]
+digits.sort()
+print(digits)
+
+for i in range(t):
+    n = int(input())
