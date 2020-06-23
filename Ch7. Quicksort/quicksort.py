9f00f11c0a3c33117635908180abbb474f5d66af
Abduaziz Kayumov <kayumovabduaziz@gmail.com>
2020-06-23 15:57:10 +0900

    added quicksort

---
+++ Ch7. Quicksort/quicksort.py
@@ -0,0 +1,41 @@
+import random
+
+
+# Quicksort is a sorting algorithm with the worst-case running time O(n^2)
+# When elements are distinct and with random partition, its expected running time O(nlgn)
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+
+
+def randomized_partition(A, p, r):
+    i = random.randint(p, r)
+    A[r], A[i] = A[i], A[r]
+    return partition(A, p, r)
+
+
+def randomized_quicksort(A, p, r):
+    if p < r:
+        q = randomized_partition(A, p, r)
+        randomized_quicksort(A, p, q - 1)
+        randomized_quicksort(A, q + 1, r)
+
+
+A = [10, 3, 15, 5, 6, 21, 1, 9]
+randomized_quicksort(A, 0, len(A) - 1)
+
+print(A)
