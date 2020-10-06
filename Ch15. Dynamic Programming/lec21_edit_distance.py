69de86d0142c86650383cabf501bdb718eb8040e
Abduaziz Kayumov <kayumovabduaziz@gmail.com>
2020-10-06 22:27:06 +0900

    edit distance

--- Ch15. Dynamic Programming/lec21_edit_distance.py
+++ Ch15. Dynamic Programming/lec21_edit_distance.py
@@ -7,23 +7,32 @@ from math import inf
 # 2) Delete x[i]
 # 3) Replace x[i] with y[j]
 
+def editDistance(w1: str, w2: str) -> int:
+    n, m = len(w1), len(w2)
+    dp = [[0] * (m + 1) for i in range(n + 1)]
 
-def lcs(x, y):
-    dp = [0] * (len(x) + 1)
+    for i in range(n+1):
+        dp[i][0] = i
 
-    for i in range(1, len(y) + 1):
+    for j in range(m+1):
+        dp[0][j] = j
 
-        column = [0] * (len(x) + 1)
-
-        for j in range(1, len(x) + 1):
-            if x[j - 1] == y[i - 1]:
-                column[j] = dp[j - 1] + 1
+    # find lcs
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if w1[i - 1] == w2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] # if cur chars are equal, cur state is equal to prev. state
             else:
-                column[j] = max(column[j - 1], dp[j])
+                # else: we need to edit, so +1 operation and min of prev.states (insert, delete or replace)
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
 
-        dp = column.copy()
-    print(dp)
+    for d in dp:
+        print(d)
+
+    return dp[-1][-1]
 
 
-lcs("HIEROGLYPHOLOGY", "MICHAELANGELO")  # HELLO
-lcs([10, 7], [10, 2, 7, 10])
+editDistance("horse", "ros")
+editDistance("intention", "execution")
+editDistance("HIEROGLYPHOLOGY", "MICHAELANGELO")  # HELLO
+editDistance([10, 7], [10, 2, 7, 10])
